bdc56a5b620476bd3ea19ee79c2aaf9134d7ced0
lonchanick <diego.arroyo2462050@gmail.com>
2025-03-24 17:07:42 -0300

    initial create

--- LAB/script.js
+++ LAB/script.js
@@ -1,35 +1,3 @@
-function decimalToRoman(num) {
-  const romanNumerals = [
-    { value: 1000, symbol: 'M' },
-    { value: 900,  symbol: 'CM' },
-    { value: 500,  symbol: 'D' },
-    { value: 400,  symbol: 'CD' },
-    { value: 100,  symbol: 'C' },
-    { value: 90,   symbol: 'XC' },
-    { value: 50,   symbol: 'L' },
-    { value: 40,   symbol: 'XL' },
-    { value: 10,   symbol: 'X' },
-    { value: 9,    symbol: 'IX' },
-    { value: 5,    symbol: 'V' },
-    { value: 4,    symbol: 'IV' },
-    { value: 1,    symbol: 'I' }
-  ];
+const arr = [1,1,1,1,1,1,1,1,1,2,2,3,4];
 
-
-  romanNumerals.map(obj => console.log(obj.value));
-
-  let result = '';
-
-  for (const { value, symbol } of romanNumerals) {
-    while (num >= value) {
-      console.log(num,'----',value,' >>> ',symbol);
-      result += symbol;
-      num -= value;
-    }
-  }
-
-  return result;
-}
-
-// Example:
-console.log(decimalToRoman(17));
+ console.log(new Set(arr).size);
