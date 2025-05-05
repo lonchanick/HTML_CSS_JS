e3d183f73df4ea6c0bf5f2f0118bc1c77ad39306
lonchanick <diego.arroyo2462050@gmail.com>
2025-05-05 13:09:38 -0400

    trying to ignore LAB directory

--- LAB/script.js
+++
@@ -1,7 +0,0 @@
-const intervalId = setInterval((name,lastname)=> {
-    console.log(name, lastname+(Math.random()+1).toFixed(2));
-}, 500, 'Diego -','Arroyo');
-
-setTimeout(()=>{
-    clearInterval(intervalId)
-}, 1000);
